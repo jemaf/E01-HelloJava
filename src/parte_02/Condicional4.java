4f035e3caae90c85b9dd9a5724ac7bc1968c9ace
Pâmela Eduarda <132526019+pamelaa777@users.noreply.github.com>
2024-04-15 08:11:52 -0300

    Update Condicional4.java
    Fiz novas alteracoes no codigo

--- src/parte_02/Condicional4.java
+++ src/parte_02/Condicional4.java
@@ -1,29 +1,30 @@
 import java.util.Scanner;
-
 public class Condicional4 {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Digite a nota do aluno (0 a 100): ");
-        int nota = scanner.nextInt();
-
-        if (nota >= 90) {
-            System.out.println("Conceito: A");
-
-        } else if (nota >= 80) {
-            System.out.println("Conceito: B");
-
-        } else if (nota >= 70) {
-            System.out.println("Conceito: C");
-
-        } else if (nota >= 60) {
-            System.out.println("Conceito: D");
-
-        } else if (nota >= 40) {
-            System.out.println("Conceito: E");
-
-        } else {
-            System.out.println("Conceito: F");
-
+    public static void main (String[] args){
+        Scanner scanner= new Scanner (System.in);
+        System.out.println("Digite a nota do aluno: ");
+        int nota= scanner.nextInt();
+        if(nota >= 90 && nota<= 100){
+            System.out.println("Tirou A");
+        }
+        if(nota >=80 && nota <=89){
+            System.out.println("Tirou B");
+        }
+        if(nota >= 70 && nota<= 79) {
+            System.out.println("Tirou C");
+        }
+        if(nota >= 60 && nota<= 69) {
+            System.out.println("Tirou D");
+        }
+        if(nota >= 40 && nota<= 59) {
+            System.out.println("Tirou E");
+        }
+        if(nota< 40) {
+            System.out.println("Tirou F");
+        }
+        else{
+            System.out.println("Erro");
         }
     }
+
 }
