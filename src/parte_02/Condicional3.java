ead8f37f29a6698882fac9e4293784fff4d84c7e
Pâmela Eduarda <132526019+pamelaa777@users.noreply.github.com>
2024-04-14 23:40:54 -0300

    Update Condicional3.java
    adicionei uma mudança ao codigo

--- src/parte_02/Condicional3.java
+++ src/parte_02/Condicional3.java
@@ -1,14 +1,16 @@
 import java.util.Scanner;
-
 public class Condicional3 {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Digite a nota do aluno (0 a 100): ");
-        int nota = scanner.nextInt();
-        if (nota >= 60) {
-            System.out.println("O aluno passou de ano.");
-        } else {
-            System.out.println("O aluno não passou de ano.");
+    public static void main (String[] args){
+        Scanner scanner= new Scanner (System.in);
+        System.out.println("Digite a nota do aluno: ");
+        int nota= scanner.nextInt();
+        if(nota >= 60 && nota<= 100){
+            System.out.println("Passou de ano");
+        }if(nota >=0 && nota < 60){
+            System.out.println("Repetiu de ano");
+        }else{
+            System.out.println("Erro");
         }
     }
+
 }
